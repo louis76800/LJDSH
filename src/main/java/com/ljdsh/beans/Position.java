2eddde368e0d7857953c85468144cc2d83e66016
louis76800 <77392391+louis76800@users.noreply.github.com>
2022-02-19 12:34:12 +0100

    projet complet

---
+++ src/main/java/com/ljdsh/beans/Position.java
@@ -0,0 +1,22 @@
+package com.ljdsh.beans;
+
+public class Position {
+    private String longitude;
+    private String lattitude;
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLattitude() {
+        return lattitude;
+    }
+
+    public void setLattitude(String lattitude) {
+        this.lattitude = lattitude;
+    }
+}
